7170fe0184063ec8c7fc308c0519c7fd06af5330
Minh Hien <minhhiena712@gmail.com>
2020-02-05 15:19:10 +0700

    add migrations

---
+++ WebBanSach_2.0.Data/WebBanSach_2_0DbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBanSach_2_0.Models;
+
+namespace WebBanSach_2_0.Data
+{
+    class WebBanSach_2_0DbContext : DbContext
+    {
+        public WebBanSach_2_0DbContext() : base("WebBanSach_2_0")
+        {
+        }
+
+        public WebBanSach_2_0DbContext Create()
+        {
+            return new WebBanSach_2_0DbContext();
+        }
+
+        public DbSet<KhachHang> KhachHangs { get; set; }
+        public DbSet<Sach> Saches { get; set; }
+        public DbSet<TacGia> TacGias { get; set; }
+        public DbSet<LoaiSach> LoaiSaches { get; set; }
+        public DbSet<NhaXuatBan> NhaXuatBans { get; set; }
+        public DbSet<BinhLuan> BinhLuans { get; set; }
+        public DbSet<ThamGia> ThamGias { get; set; }
+        public DbSet<DonHangBan> DonHangBans { get; set; }
+        public DbSet<ChiTietDonBan> ChiTietDonBans { get; set; }
+        public DbSet<DonHangNhap> DonHangNhaps { get; set; }
+        public DbSet<ChiTietDonNhap> ChiTietDonNhaps { get; set; }
+        public DbSet<NhanVien> NhanViens { get; set; }
+        public DbSet<ChucVu> ChucVus { get; set; }
+
+    }
+}
